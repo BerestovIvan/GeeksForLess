dca481b4908b2ed5f753f7b1c5d098177a1a4738
Berestov <berestovvanya@gmail.com>
2022-03-25 13:06:30 +0200

    Add project files.

---
+++ GeeksForLess/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GeeksForLess.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
